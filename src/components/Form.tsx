33917989a1897cc926b062cadbbc5cd3964d2732
JyojiTenguam <j.y.oji@hotmail.com>
2024-03-02 20:15:53 -0300

    Resolvendo requisito 03 de ProjectPassword Manager

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+type FormProps = {
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void; // * funcao que recebe event e retorna nada
+  handleInputFocus: (event: React.FocusEvent<HTMLInputElement>) => void; // * funcao que recebe event e retorna nada
+  handleSendBtnClick: (event: React.MouseEvent<HTMLButtonElement>) => void; // * funcao que recebe event e retorna nada
+  data: { // * objeto
+    'service-name': string;
+    login: string;
+    password: string;
+    url: string;
+  };
+  validateForm: () => boolean; // * funcao que recebe nada e retorna true ou false (boolean)
+};
 
 function Form() {
+  const [isButtonVisible, setIsButtonVisible] = useState(true);
+  const [isFormVisible, setIsFormVisible] = useState(true);
+
   return (
-    <form>
-      <label htmlFor="service-name">Nome do Serviço</label>
-      <input id="service-name" type="text" />
+    <>
+      {isFormVisible && (
+        <form>
+          <label htmlFor="service-name">Nome do Serviço</label>
+          <input id="service-name" type="text" />
 
-      <label htmlFor="login">Login</label>
-      <input id="login" type="text" />
+          <label htmlFor="login">Login</label>
+          <input id="login" type="text" />
 
-      <label htmlFor="password">Senha</label>
-      <input id="password" type="password" />
+          <label htmlFor="password">Senha</label>
+          <input id="password" type="password" />
 
-      <label htmlFor="url">URL</label>
-      <input id="url" type="text" />
+          <label htmlFor="url">URL</label>
+          <input id="url" type="text" />
 
-      <button>Cadastrar</button>
-      <button>Cancelar</button>
-    </form>
+          <button>Cadastrar</button>
+          <button onClick={() => {
+            setIsFormVisible(false);
+            setIsButtonVisible(true);
+          }}>Cancelar</button>
+        </form>
+      )}
+      {isButtonVisible && (
+        <button onClick={() => setIsButtonVisible(false)}>Cadastrar nova senha</button>
+      )}
+    </>
   );
 }
 
